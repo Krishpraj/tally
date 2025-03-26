b8a73044b42f5da1126a075cd428dd315c8ce7bb
Krishpraj <krishpraj6@gmail.com>
2025-03-26 19:19:49 -0400

    Replace Inter font with Roboto for improved ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 import { Providers } from '@/components/providers'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Roboto({ weight: '400', subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Tally',
